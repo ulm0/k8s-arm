e6ff3d8e8b6fc145e7bbac514e70eabeb06321e1
Pierre Ugaz <pierre.ugazm@gmail.com>
2017-10-24 02:50:38 -0300

    update script and readme

--- README.md
+++ README.md
@@ -2,9 +2,43 @@
 
 This tiny script was created to easily install Docker + Kubernetes on *a fresh install* (recommended) Raspbian/HypriotOS.
 
-You can clone this repo into your RPi or simply run
+It installs [kubeadm](https://kubernetes.io/docs/setup/independent/install-kubeadm/#installing-kubeadm-kubelet-and-kubectl), one of the recommended docker versions (as mentioned in [install-kubeadm/#installing-docker](https://kubernetes.io/docs/setup/independent/install-kubeadm/#installing-docker)) and then sets on hold docker to avoid being upgraded whenever `apt-get upgrade` is executed.
 
-`wget -qO - https://gitlab.com/klud/k8s-arm/raw/master/install.sh | sudo bash -`
+## Usage
+
+* Simply run `wget -qO - https://gitlab.com/klud/k8s-arm/raw/master/install.sh | sudo bash -`
+
+* Or clone this repo into your RPi, grant execution permission `chmod +x install.sh`. **It must run as root or sudo.**
+
+### Options
+
+```sh
+    -h            Print this help
+    -v            Print version info
+    -l            List available versions
+    -i [version]  Specify docker version to install
+```
+
+**NOTE:** if `-i` is not used, a default version *(1.12.6)* will be installed.
+
+### Installing a given version
+
+All you need to do is choose one version and use it as follows `install.sh -i 17.03.0~ce-0~raspbian-jessie`.
+
+#### List of available versions
+
+* 17.05.0~ce-0~raspbian-jessie (Not tested)
+* 17.04.0~ce-0~raspbian-jessie (Not tested)
+* 17.03.1~ce-0~raspbian-jessie (Might work)
+* 17.03.0~ce-0~raspbian-jessie (Recommended)
+* 1.13.1-0~raspbian-jessie (Recommended)
+* 1.13.0-0~raspbian-jessie (Recommended)
+* 1.12.6-0~raspbian-jessie (Recommended | **Default**)
+* 1.12.5-0~raspbian-jessie (Recommended)
+* 1.12.4-0~raspbian-jessie (Recommended)
+* 1.12.3-0~jessie (Recommended)
+* 1.12.2-0~jessie (Recommended)
+* 1.12.1-0~jessie (Recommended)
 
 ## More info about Kubernetes and ARM
 
--- install.sh
+++ install.sh
@@ -1,63 +1,168 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 set -e
 
-_echo(){
-    echo ${@}
+function print_version(){
+    echo "K8s for ARM v0.0.1"
+    exit 1
 }
 
-_tee(){
-    tee ${@}
+function print_help() {
+    echo "Usage: $0"
+    echo "    -h            Print this help"
+    echo "    -v            Print version info"
+    echo "    -l            List available versions"
+    echo "    -i [version]  Specify docker version to install"
+    echo
+    echo "You must specify a docker version. See:"
+    echo "https://github.com/untalpierre/k8s-arm#usage"
+    exit 1
 }
 
-_curl(){
-    curl ${@}
+function docker_versions(){
+    info "Docker" "List of available versions"
+    echo "  - 17.05.0~ce-0~raspbian-jessie      | Not tested"
+    echo "  - 17.04.0~ce-0~raspbian-jessie      | Not tested"
+    echo "  - 17.03.1~ce-0~raspbian-jessie      | Might work"
+    echo "  - 17.03.0~ce-0~raspbian-jessie      | Recommended"
+    echo "  - 1.13.1-0~raspbian-jessie          | Recommended"
+    echo "  - 1.13.0-0~raspbian-jessie          | Recommended"
+    echo "  - 1.12.6-0~raspbian-jessie          | Recommended | Default"
+    echo "  - 1.12.5-0~raspbian-jessie          | Recommended"
+    echo "  - 1.12.4-0~raspbian-jessie          | Recommended"
+    echo "  - 1.12.3-0~jessie                   | Recommended"
+    echo "  - 1.12.2-0~jessie                   | Recommended"
+    echo "  - 1.12.1-0~jessie                   | Recommended"
+    exit 1
 }
 
-update(){
-    _echo "Updating..."
+# Display an error message and quit
+function bail() {
+    FG="1;31m"
+    BG="40m"
+    echo -en "[\033[${FG}\033[${BG}Error\033[0m] "
+    echo "$*"
+    exit 1
+}
+
+# Display an info message
+function info() {
+    task="$1"
+    shift
+    FG="1;32m"
+    BG="40m"
+    echo -e "[\033[${FG}\033[${BG}${task}\033[0m] $*"
+}
+
+# Refresh repos
+function update(){
+    info "Updating..."
     apt-get -qq update
 }
 
-get_keys(){
-    _echo -e "\033[1mGetting key for \033[32m${1}\033[0m"
-    _curl -fsSL "${1}" | apt-key add -
+# Get GPG Keys
+function get_keys(){
+    info "Keys" "Getting key for ${1}"
+    curl -fsSL "${1}" | apt-key add -
 }
 
-add_repo(){
-    _echo -e "\033[1mAdding repo to \033[32m${1}\033[0m"
-    _echo "${2}" | tee "${1}"
+# Add a given repo
+function add_repo(){
+    info "Repo" "Adding to ${1}"
+    echo "${2}" | tee "${1}"
 }
 
-install_docker(){
-    if [ -s /etc/apt/sources.list.d/docker.list ]; then
-        rm -f /etc/apt/sources.list.d/docker.list
+# Install Docker
+function install_docker(){
+    if [[ -s $(which docker) ]]; then
+        info "Docker" "Previous installation found..."
+        remove_prev_docker
     fi
     get_keys "https://apt.dockerproject.org/gpg"
     add_repo "/etc/apt/sources.list.d/docker.list" "deb [arch=armhf] https://apt.dockerproject.org/repo raspbian-jessie main"
     update
-    _echo "Installing Docker..."
-    apt-get install -qq -y docker-engine=17.03.1~ce-0~raspbian-jessie
-    _echo "Setting hold docker-engine..."
+    info "Docker" "Installing..."
+    apt-get install -qq -y docker-engine=${1}
+    info "Docker" "Setting on hold..."
     apt-mark -qq hold docker-engine
 }
 
-install_kubernetes(){
+# Look for previous docker installation and remove it
+function remove_prev_docker(){
+    info "Docker" "Removing existing installation..."
+    apt-get -qq -y remove docker docker-ce docker.io docker-engine
+    apt-get -qq -y autoremove docker docker-ce docker.io docker-engine
+    apt-get -qq -y purge docker docker-ce docker.io docker-engine
+    if [[ -s /etc/apt/sources.list.d/docker.list ]]; then
+        info "Docker" "Removing old source repo..."
+        rm -f /etc/apt/sources.list.d/docker.list
+    fi
+}
+
+# Install K8s
+function install_kubernetes(){
     get_keys "https://packages.cloud.google.com/apt/doc/apt-key.gpg"
     add_repo "/etc/apt/sources.list.d/kubernetes.list" "deb http://apt.kubernetes.io/ kubernetes-xenial main"
     update
+    info "Kubernetes" "Installing..."
     apt-get install -qq -y kubelet kubeadm kubectl
 }
 
-_echo "This will install Kubernetes & Docker on your Raspbian/HypriotOS system"
-update
-_echo "Installing dependencies..."
-apt-get install -qq -y apt-transport-https
-
-if [ -s $(which docker) ]; then
-    _echo "Removing previous docker installation..."
-    apt-get -qq -y remove docker-ce docker.io docker-engine
+if [[ $EUID -ne 0 ]]; then
+    bail "must be run as root. try: sudo install.sh"
 fi
-install_docker
-install_kubernetes
-_echo "Done"
+
+# Handle arguments:
+args=$(getopt -uo 'hvli:' -- $*)
+[ $? != 0 ] && print_help
+set -- $args
+
+for i
+do
+    case "$i"
+    in
+        -h)
+            print_help
+            ;;
+        -v)
+            print_version
+            ;;
+        -l)
+            docker_versions
+            ;;
+        -i)
+            docker_version="$2"
+            echo "Docker version = ${2}"
+            shift
+            shift
+            ;;
+    esac
+done
+
+if [[ -z "$docker_version" ]]; then
+    info "Start" "No docker version specified, using default"
+    docker_version="1.12.6-0~raspbian-jessie"
+fi
+
+info "Start" "Will remove any docker installed on your system"
+info "Start" "and install a suitable docker version for kubernetes"
+read -p "Really proceed? (y)es / (n)o " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    echo "Quitting."
+    exit
+fi
+
+    update
+info "Deps" "Installing apt-transport-https..."
+    apt-get install -qq -y apt-transport-https
+    install_docker "${docker_version}"
+    install_kubernetes
+info "Info" "Docker and Kuberntes have been installed"
+info "Info" "You might want to check:"
+info "Info" "docker version"
+info "Info" "kubectl version"
+info "Info" "kubeadm version"
+info "Info" "kubelet version"
+info "Info" "Enjoy...!"
